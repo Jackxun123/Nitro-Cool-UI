59db0d9d243cb194df3f396ff5dd04b8ec9f27c2
Jackxun <33611532+Jackxun123@users.noreply.github.com>
2025-05-26 02:42:07 +0800

    Update WiredActionBotGiveHandItemView.tsx
    修改手持物

--- src/components/wired/views/actions/WiredActionBotGiveHandItemView.tsx
+++ src/components/wired/views/actions/WiredActionBotGiveHandItemView.tsx
@@ -4,7 +4,7 @@ import { Column, Text } from '../../../../common';
 import { useWired } from '../../../../hooks';
 import { WiredActionBaseView } from './WiredActionBaseView';
 
-const ALLOWED_HAND_ITEM_IDS: number[] = [ 2, 5, 7, 8, 9, 10, 27 ];
+const ALLOWED_HAND_ITEM_IDS: number[] = [ 2, 5, 7, 8, 9, 10, 27, 1081, 29, 1058, 1077, 148, 1094];
 
 export const WiredActionBotGiveHandItemView: FC<{}> = props =>
 {
